3ffa5d11cc7d9c6b579664f3f9519dad9ded0b45
Reese Mallory <mrm90975@uga.edu>
2021-12-14 22:55:19 -0500

    added sql file

---
+++ petstore4370.sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2021 at 04:54 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `petstore4370`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_ID` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_ID`, `user_ID`) VALUES
+(1, 1),
+(2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cartitems`
+--
+
+CREATE TABLE `cartitems` (
+  `cart_ID` int(11) NOT NULL,
+  `prod_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cartitems`
+--
+
+INSERT INTO `cartitems` (`cart_ID`, `prod_ID`) VALUES
+(1, 1);
+
+--
+-- Triggers `cartitems`
+--
+DELIMITER $$
+CREATE TRIGGER `add qty` AFTER DELETE ON `cartitems` FOR EACH ROW UPDATE products SET qty = qty + 1 WHERE prod_ID = old.prod_ID
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `subtract qty` AFTER INSERT ON `cartitems` FOR EACH ROW UPDATE products SET qty = qty - 1 WHERE prod_ID = new.prod_ID
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_ID` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL,
+  `cart_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `user_ID` int(11) NOT NULL,
+  `payment_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paymentinfo`
+--
+
+CREATE TABLE `paymentinfo` (
+  `payment_ID` int(11) NOT NULL,
+  `CardNum` bigint(16) NOT NULL,
+  `ExpDate` varchar(50) NOT NULL,
+  `CVV` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `paymentinfo`
+--
+
+INSERT INTO `paymentinfo` (`payment_ID`, `CardNum`, `ExpDate`, `CVV`) VALUES
+(1, 1231231231231235, '08/22', 123),
+(2, 1231231231231234, '10/26', 123);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `prod_ID` int(11) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `Category` varchar(20) NOT NULL,
+  `Description` varchar(250) NOT NULL,
+  `Image` varchar(250) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`prod_ID`, `Name`, `Price`, `Category`, `Description`, `Image`, `qty`) VALUES
+(1, '', '0.00', '', '', '', 0),
+(2, 'Doggy', '9.00', 'gadgets', 'this is doge', 'Courseoof.PNG', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_ID` int(11) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `phoneNum` int(11) DEFAULT NULL,
+  `Address` varchar(75) DEFAULT NULL,
+  `State` varchar(20) DEFAULT NULL,
+  `Country` varchar(30) DEFAULT NULL,
+  `ZIP` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_ID`, `email`, `password`, `firstname`, `lastname`, `phoneNum`, `Address`, `State`, `Country`, `ZIP`) VALUES
+(1, 'test@test', 'test', 'Reese', 'Mallory', 2147483647, '118 Ruth Drive', 'GA', 'US', '30601'),
+(2, 'test@test.com', 'test', 'test', 'test', NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `create cart` AFTER INSERT ON `users` FOR EACH ROW INSERT INTO cart (cart_ID, user_ID)VALUES(new.user_ID,new.user_ID)
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_ID`),
+  ADD KEY `user_ID` (`user_ID`);
+
+--
+-- Indexes for table `cartitems`
+--
+ALTER TABLE `cartitems`
+  ADD PRIMARY KEY (`cart_ID`,`prod_ID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `cart_ID` (`cart_ID`),
+  ADD KEY `user_ID` (`user_ID`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`user_ID`,`payment_ID`);
+
+--
+-- Indexes for table `paymentinfo`
+--
+ALTER TABLE `paymentinfo`
+  ADD PRIMARY KEY (`payment_ID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`prod_ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cart_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `paymentinfo`
+--
+ALTER TABLE `paymentinfo`
+  MODIFY `payment_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `prod_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `users` (`user_ID`);
+
+--
+-- Constraints for table `cartitems`
+--
+ALTER TABLE `cartitems`
+  ADD CONSTRAINT `cartitems_ibfk_2` FOREIGN KEY (`cart_ID`) REFERENCES `cart` (`cart_ID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`cart_ID`) REFERENCES `cart` (`cart_ID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user_ID`) REFERENCES `users` (`user_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
